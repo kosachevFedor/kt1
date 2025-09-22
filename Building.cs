0757b555248458633894ac3076e8832fef7b1b2e
fedor <90831517+kosachevFedor@users.noreply.github.com>
2025-09-22 18:32:25 +0300

    Create Building.cs

---
+++ Building.cs
@@ -0,0 +1,11 @@
+class Building
+{
+    private string _address;
+
+    public Building(string address)
+    {
+        _address = address;
+    }
+
+    public string Address { get { return _address; } }
+}
